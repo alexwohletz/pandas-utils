a951ee0df1bd978fd0be3cf5639b6f5f90d61ef8
Alex Wohletz <awohletz3@gmail.com>
2020-02-23 20:14:15 -0800

    added function documentation

--- utils.py
+++ utils.py
@@ -1,4 +1,24 @@
 def update_join(left_df, right_df, on, right_col, fillna=True, validate=True):
+    """Simple function to mimic a SQL update set col1 = col2 from df1 join df2 on df1.key == df2.key. Does not modify dataframes in place.
+    
+    Arguments:
+        left_df {dataframe} -- Left Dataframe.
+        right_df {dataframe} -- Right Dataframe.
+        on {str} -- Key or shared column to 'join' both dataframes on.
+        right_col {str} -- Col2, the column from which we are updating column 1.
+    
+    Keyword Arguments:
+        fillna {bool} -- Whether or not to fill gaps in Col1. (default: {True})
+        validate {bool} -- Checks to make sure the indexes share values and warn if something is off. (default: {True})
+    
+    Raises:
+        KeyError: No shared key.
+        KeyError: No shared indexes in that key.
+        KeyError: Right column does not exist in right dataframe.
+    
+    Returns:
+        pd.Series -- A pandas Series with the length of left dataframe updated with the values of the right column based on the key.
+    """
     import warnings
 
     left_df = left_df.copy()
